27845fb07108c110c1a4bb69e79b79c7ae4bc3a4
rvarszegi <r.varszegi93@gmail.com>
2019-02-18 21:31:27 +0100

    Error handling next

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,18 +3,25 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web apiék
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HerosService: fetched heroes');
-    return of (HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     this.messageService.add('HeroService: fetched hero id=${id}');
     return of (HEROES.find(hero => hero.id === id));
   }
-  constructor(private messageService: MessageService) { }
+  private log(message: string) {
+    this.messageService.add('HeroService: ${message}');
+  }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
 }
