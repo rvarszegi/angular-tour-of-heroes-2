524da64e1d4897fa9d12bbfeb9d7a0684724e789
rvarszegi <r.varszegi93@gmail.com>
2019-02-19 22:10:06 +0100

    finished

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,6 +20,24 @@ export class HeroService {
     const url = '$this.heroUrl/${id}';
     return this.http.get<Hero>(url).pipe(tap(_ => this.log('fetched hero id=${id}')));
   }
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(tap((newHero: Hero) => this.log('added hero w/ id=${newHero.id}')),
+    catchError(this.handleError<Hero>('addHero')));
+  }
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = '${this.heroesUrl}/${id}';
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(tap(_ => this.log('deleted hero id=${id}')),
+    catchError(this.handleError<Hero>('deleteHero')));
+  }
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>('${this.heroesUrl}/? name=${term}').pipe(tap(_ => this.log('found heroes matching "${term}"')),
+    catchError(this.handleError<Hero[]>('searchHeroes', [])));
+    }
   private log(message: string) {
     this.messageService.add('HeroService: ${message}');
   }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +13,15 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   getHeroes() {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if(!name) {return;}
+    this.heroService.addHero({ name } as Hero).subscribe(hero => { this.heroes.push(hero); });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
   constructor(private heroService: HeroService) { }
 
